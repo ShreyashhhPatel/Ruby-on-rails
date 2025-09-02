eef21f04a99d2700cf4bc83bd873cc2d02da4653
Shreyash V <Shreyashhhpatel@users.noreply.github.com>
2025-09-02 10:57:28 -0400

    Initial commit, readme and some basic syntax

---
+++ calc.rb
@@ -0,0 +1,16 @@
+puts 1 + 2
+puts 24 * 7 * 4 * 12
+puts 'hello ' + 'world'
+puts 'hello ' * 3
+# puts '12' * 3
+# puts '12' + '3'
+# puts '12' + 3.to_s  
+# puts '12' + 3
+# puts '12' * 3
+
+var = 'Shreyash ' + 'Patel '
+puts var 
+puts 'my name is ' + var
+
+var = 3 * 6
+puts var
